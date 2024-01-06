00579084942f1bf99c72327e61c5d4ce4560c64b
cinos81 <cinos81@gmail.com>
2024-01-06 13:52:38 +0900

    1차 마이그레이션

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+new Vue({
+  render: (h) => h(App)
+}).$mount('#app')
