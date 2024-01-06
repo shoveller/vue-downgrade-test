cc8cf49042bf4935adecb5e3c666e3392bf5867c
cinos81 <cinos81@gmail.com>
2024-01-06 14:25:35 +0900

    pinia 설치

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import './assets/main.css'
 import Vue from 'vue'
 import App from './App.vue'
 import Fragment from 'vue-fragment'
+import { createPinia } from 'pinia'
 
 Vue.use(Fragment.Plugin)
 
 new Vue({
+  pinia: createPinia(),
   render: (h) => h(App)
 }).$mount('#app')
